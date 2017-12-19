b0ddb0da490b36c682723e2e97713a0ba63209ed
jorwalk <jorwalk@gmail.com>
2017-12-19 10:05:09 -0600

    webpack react starter

---
+++ front-end/react/react-webpack-starter/src/client/app/app.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from 'react-dom'
+
+class App extends React.Component {
+  render() {
+
+    return(
+      <header>
+         <h1>Heading One</h1>
+      </header>
+    )
+  }
+}
+
+// const container = window.document.createElement("article")
+// container.setAttribute("id", "root");
+// document.body.appendChild(container)
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
