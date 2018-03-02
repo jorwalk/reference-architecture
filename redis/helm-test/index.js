cd2097a735ad615a1b6f9f3cac92df469d51ff7b
jorwalk <jorwalk@gmail.com>
2018-03-02 13:36:33 -0600

    add retry function

--- redis/helm-test/index.js
+++ redis/helm-test/index.js
@@ -1,7 +1,23 @@
-let redis = require("redis")
-let assert = require("assert")
+let redis = require("redis"),
+    assert = require("assert")
+
+let Retry = function(options) {
+    if (options.error && options.error.code === 'ECONNREFUSED') {
+        return new Error('The server refused the connection');
+    }
+    if (options.total_retry_time > 1000 * 60 * 60) {
+        return new Error('Retry time exhausted');
+    }
+    if (options.attempt > 10) {
+        // End reconnecting with built in error
+        return undefined;
+    }
+    // reconnect after
+    return Math.min(options.attempt * 100, 3000);
+}
+
 let Redis = function() {
-  this.client = redis.createClient({ host: '0.0.0.0' })
+  this.client = redis.createClient({ 'retry_strategy': Retry })
 }
 
 Redis.prototype = {
@@ -12,10 +28,10 @@ Redis.prototype = {
     this.client.get(key, cb)
   },
   set: function(key, value) {
-    this.client.set(key, value, redis.print)
+    this.client.set(key, value, null)
   },
   hset: function(key, values) {
-    this.client.hset(key, values, redis.print);
+    this.client.hset(key, values, null);
   },
   delete: function (key) {
     this.client.del(key)
@@ -34,13 +50,13 @@ r.on("error", () => { assert.fail("cb: error event") })
 r.on("end", () => { assert("cb: end event") })
 r.on("warning", () => { assert.fail("cb: warning event") })
 r.set("urn:key:test", "test:value")
-// r.get("urn:key:test", (err, reply) => {
-//   if(err) assert.fail("cb: get key error")
-//   assert.equal(reply.toString(), "test:value")
-// })
-// r.delete("urn:key:test")
-// r.get("urn:key:test", (err, reply) => {
-//   if(err) assert("cb: key does not exist")
-//   assert.equal(reply, null)
-// })
+r.get("urn:key:test", (err, reply) => {
+  if(err) assert.fail("cb: get key error")
+  assert.equal(reply.toString(), "test:value")
+})
+r.delete("urn:key:test")
+r.get("urn:key:test", (err, reply) => {
+  if(err) assert("cb: key does not exist")
+  assert.equal(reply, null)
+})
 r.quit()
