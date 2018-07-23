2e484529dd7d8e923af4476532d14f1da6981e97
jorwalk <jorwalk@gmail.com>
2018-07-23 08:37:37 -0500

    vgg keras

---
+++ python/vggkeras.py
@@ -0,0 +1,225 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import Flatten
+from keras.constraints import maxnorm
+from keras.optimizers import SGD
+from keras.layers import Activation
+from keras.layers.convolutional import Conv2D, MaxPooling2D, ZeroPadding2D
+from keras.layers.normalization import BatchNormalization
+from keras.initializers import glorot_normal
+from keras.utils import np_utils
+from keras_sequential_ascii import sequential_model_to_ascii_printout
+from keras import backend as K
+if K.backend()=='tensorflow':
+    K.set_image_dim_ordering("th")
+
+# Import Tensorflow with multiprocessing
+import tensorflow as tf
+import multiprocessing as mp
+
+core_num = mp.cpu_count()
+print(core_num)
+config = tf.ConfigProto(
+    inter_op_parallelism_threads=core_num,
+    intra_op_parallelism_threads=core_num)
+sess = tf.Session(config=config)
+
+# Loading the CIFAR-10 dataset
+
+from keras.datasets import cifar10
+
+# Declare variables
+
+BATCH_NORM = False
+
+batch_size = 64
+num_classes = 10
+epochs = 25
+data_augmentation = True
+
+(x_train, y_train), (x_test, y_test) = cifar10.load_data() # x_train - training data(images), y_train - labels(digits)
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+
+class_names = ['airplane','automobile','bird','cat','deer',
+               'dog','frog','horse','ship','truck']
+
+
+fig = plt.figure(figsize=(8,3))
+for i in range(num_classes):
+    ax = fig.add_subplot(2, 5, 1 + i, xticks=[], yticks=[])
+    idx = np.where(y_train[:]==i)[0]
+    features_idx = x_train[idx,::]
+    img_num = np.random.randint(features_idx.shape[0])
+    im = np.transpose(features_idx[img_num,::],(1,2,0))
+    ax.set_title(class_names[i])
+    plt.imshow(im)
+plt.show()
+
+# Convert and pre-processing
+
+y_train = np_utils.to_categorical(y_train, num_classes)
+y_test = np_utils.to_categorical(y_test, num_classes)
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train  /= 255
+x_test /= 255
+
+
+# Define Model
+def base_model():
+    model = Sequential()
+
+    model.add(Conv2D(64, (3, 3), padding='same', input_shape=x_train.shape[1:], name='block1_conv1'))
+    model.add(BatchNormalization()) if BATCH_NORM else None
+    model.add(Activation('relu'))
+
+    model.add(Conv2D(64, (3, 3), padding='same', name='block1_conv2'))
+    model.add(BatchNormalization()) if BATCH_NORM else None
+    model.add(Activation('relu'))
+
+    model.add(MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool'))
+
+    model.add(Conv2D(128, (3, 3), padding='same', name='block2_conv1'))
+    model.add(BatchNormalization()) if BATCH_NORM else None
+    model.add(Activation('relu'))
+
+    model.add(Conv2D(128, (3, 3), padding='same', name='block2_conv2'))
+    model.add(BatchNormalization()) if BATCH_NORM else None
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool'))
+
+    model.add(Conv2D(256, (3, 3), padding='same', name='block3_conv1'))
+    model.add(BatchNormalization()) if BATCH_NORM else None
+    model.add(Activation('relu'))
+
+    model.add(Conv2D(256, (3, 3), padding='same', name='block3_conv2'))
+    model.add(BatchNormalization()) if BATCH_NORM else None
+    model.add(Activation('relu'))
+
+    model.add(Conv2D(256, (3, 3), padding='same', name='block3_conv3'))
+    model.add(BatchNormalization()) if BATCH_NORM else None
+    model.add(Activation('relu'))
+
+    model.add(Conv2D(256, (3, 3), padding='same', name='block3_conv4'))
+    model.add(BatchNormalization()) if BATCH_NORM else None
+    model.add(Activation('relu'))
+
+    model.add(MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool'))
+
+    model.add(Conv2D(512, (3, 3), padding='same', name='block4_conv1'))
+    model.add(BatchNormalization()) if BATCH_NORM else None
+    model.add(Activation('relu'))
+
+    model.add(Conv2D(512, (3, 3), padding='same', name='block4_conv2'))
+    model.add(BatchNormalization()) if BATCH_NORM else None
+    model.add(Activation('relu'))
+
+    model.add(Conv2D(512, (3, 3), padding='same', name='block4_conv3'))
+    model.add(BatchNormalization()) if BATCH_NORM else None
+    model.add(Activation('relu'))
+
+    model.add(Conv2D(512, (3, 3), padding='same', name='block4_conv4'))
+    model.add(BatchNormalization()) if BATCH_NORM else None
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool'))
+
+    model.add(Conv2D(512, (3, 3), padding='same', name='block5_conv1'))
+    model.add(BatchNormalization()) if BATCH_NORM else None
+    model.add(Activation('relu'))
+
+    model.add(Conv2D(512, (3, 3), padding='same', name='block5_conv2'))
+    model.add(BatchNormalization()) if BATCH_NORM else None
+    model.add(Activation('relu'))
+
+    model.add(Conv2D(512, (3, 3), padding='same', name='block5_conv3'))
+    model.add(BatchNormalization()) if BATCH_NORM else None
+    model.add(Activation('relu'))
+
+    model.add(Conv2D(512, (3, 3), padding='same', name='block5_conv4'))
+    model.add(BatchNormalization()) if BATCH_NORM else None
+    model.add(Activation('relu'))
+
+    model.add(Flatten())
+
+    model.add(Dense(4096))
+    model.add(BatchNormalization()) if BATCH_NORM else None
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+
+    model.add(Dense(4096, name='fc2'))
+    model.add(BatchNormalization()) if BATCH_NORM else None
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+
+    model.add(Dense(num_classes))
+    model.add(BatchNormalization()) if BATCH_NORM else None
+    model.add(Activation('softmax'))
+
+    sgd = SGD(lr=0.0005, decay=0, nesterov=True)
+
+    model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+    return model
+
+cnn_n = base_model()
+cnn_n.summary()
+
+cnn = cnn_n.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_data=(x_test,y_test), shuffle=True)
+
+# Vizualizing model structure
+
+sequential_model_to_ascii_printout(cnn_n)
+
+# Plots for trainng and testing process: loss and accuracy
+
+plt.figure(0)
+plt.plot(cnn.history['acc'],'r')
+plt.plot(cnn.history['val_acc'],'g')
+plt.xticks(np.arange(0, 11, 2.0))
+plt.rcParams['figure.figsize'] = (8, 6)
+plt.xlabel("Num of Epochs")
+plt.ylabel("Accuracy")
+plt.title("Training Accuracy vs Validation Accuracy")
+plt.legend(['train','validation'])
+
+plt.figure(1)
+plt.plot(cnn.history['loss'],'r')
+plt.plot(cnn.history['val_loss'],'g')
+plt.xticks(np.arange(0, 11, 2.0))
+plt.rcParams['figure.figsize'] = (8, 6)
+plt.xlabel("Num of Epochs")
+plt.ylabel("Loss")
+plt.title("Training Loss vs Validation Loss")
+plt.legend(['train','validation'])
+plt.show()
+
+scores = cnn_n.evaluate(x_test, y_test, verbose=0)
+print("Accuracy: %.2f%%" % (scores[1]*100))
+
+
+# Confusion matrix result
+
+from sklearn.metrics import classification_report, confusion_matrix
+Y_pred = cnn_n.predict(x_test, verbose=2)
+y_pred = np.argmax(Y_pred, axis=1)
+
+for ix in range(10):
+    print(ix, confusion_matrix(np.argmax(y_test,axis=1),y_pred)[ix].sum())
+cm = confusion_matrix(np.argmax(y_test,axis=1),y_pred)
+print(cm)
+
+# Visualizing of confusion matrix
+import seaborn as sn
+import pandas  as pd
+
+
+df_cm = pd.DataFrame(cm, range(10),
+                  range(10))
+plt.figure(figsize = (10,7))
+sn.set(font_scale=1.4)#for label size
+sn.heatmap(df_cm, annot=True,annot_kws={"size": 12})# font size
+plt.show()
